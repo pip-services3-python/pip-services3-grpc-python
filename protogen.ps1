08278a41e4010c18299f46e21a663f68a3b1814e
Dmitriy Krayniy <krdima92@gmail.com>
2020-12-28 10:23:21 +0300

    updated pipeline. use github acions

--- protogen.ps1
+++ protogen.ps1
@@ -1,12 +1,12 @@
 #!/usr/bin/env pwsh
 
-##Set-StrictMode -Version latest
+Set-StrictMode -Version latest
 $ErrorActionPreference = "Stop"
 
 # Get component data and set necessary variables
 $component = Get-Content -Path "component.json" | ConvertFrom-Json
 
-$docsImage="$($component.registry)/$($component.name):$($component.version)-$($component.build)-proto"
+$protoImage="$($component.registry)/$($component.name):$($component.version)-$($component.build)-proto"
 $container=$component.name
 
 # Remove old generate files
@@ -19,10 +19,10 @@ if (Test-Path "test/protos") {
 }
 
 # Build docker image
-docker build --build-arg COMPONENT_NAME="$($component.name)" -f docker/Dockerfile.proto -t $docsImage .
+docker build --build-arg COMPONENT_NAME="$($component.name)" -f docker/Dockerfile.proto -t $protoImage .
 
 # Create and copy compiled files, then destroy
-docker create --name $container $docsImage
+docker create --name $container $protoImage
 docker cp "$($container):/app/$($component.name)/protos" ./"$($component.name)"/
 docker cp "$($container):/app/test/protos" ./test/
 docker rm $container
